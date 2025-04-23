25d003df7ed661720b9713aad86cab8563db3472
yugal1233 <39340134+yugal1233@users.noreply.github.com>
2025-04-23 19:01:23 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Space Portfolio",
+  description: "This is my portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Hero from "@/components/main/Hero";
+import Image from "next/image";
+import Skills from "@/components/main/Skills";
+import Encryption from "@/components/main/Encryption";
+import Projects from "@/components/main/Projects";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+      <Hero />
+      <Skills />
+      <Encryption />
+      <Projects />
+      </div>
+    </main>
+  );
+}
